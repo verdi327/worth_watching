6837d88264ab1093edf34d1a8fad2d793999009e
michael verdi <michael.v.verdi@gmail.com>
2012-05-03 15:23:58 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WorthWatching::Application.config.secret_token = '6271c871a002af66db6f5a8628144c93c48fbc265e20033624381b6d3eaafe784bb4ab6fb2363171eb7f690d5c0d60d2bea9de0ff66cf20078390f7797ff0ba8'
